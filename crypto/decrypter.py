3f5f28457685a78ed0bf88563227d577b72f86c5
Chanours <valentin.machefaux@gmail.com>
2024-12-19 17:06:10 +0100

    -Feat Encryption and decryption finished

---
+++ crypto/decrypter.py
@@ -0,0 +1,41 @@
+import os
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import unpad
+
+
+with open("key.bin", 'rb') as f:
+    key = f.read()
+
+folder = "../dossier_confidentiel"
+
+
+def decrypt_file(path, key, output_path=None):
+
+    if output_path is None:
+        output_path = path.replace('.enc', '')
+
+    with open(path, 'rb') as f:
+
+        iv = f.read(16)
+        ciphertext = f.read()
+
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    data = unpad(cipher.decrypt(ciphertext), AES.block_size)
+
+
+    with open(output_path, 'wb') as f:
+        f.write(data)
+
+    print(f"File {path} successfully decrypted in {output_path}")
+
+def decrypt_folder(folder_path, key):
+
+    for root, _, files in os.walk(folder_path):
+        for file in files:
+            if file.endswith('.enc'):
+                file_path = os.path.join(root, file)
+                decrypt_file(file_path, key)
+                os.remove(file_path)
+    os.remove("key.bin")
+
+decrypt_folder(folder,key)
---
+++ crypto/encrypter.py
@@ -0,0 +1,50 @@
+import os
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
+
+key = get_random_bytes(32)
+with open("key.bin", "wb") as f:
+    # Générer une clé AES de 256 bits
+    f.write(key)
+folder = "../dossier_confidentiel"
+
+
+def encrypt_file(path,key,output_path =None):
+    try:
+
+        if output_path is None:
+            output_path = path + '.enc'
+
+        with open(path, 'rb') as f:
+            data = f.read()
+
+        # iv = vecteur d'initialisation
+        # En gros, c'est une value aléatoire utiliser pour le chiffrement de même données avec la même clé mais on y sorte des données diff
+
+        iv = get_random_bytes(16)
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        ciphertext = cipher.encrypt(pad(data, AES.block_size))
+
+        with open(output_path, 'wb') as f:
+            f.write(iv + ciphertext)
+
+        print(f"File {path} successfully crypted in {output_path}")
+
+    except Exception as e:
+        print(e)
+
+def encrypt_folder(folder_path, key):
+    try:
+        print(folder_path)
+        for root, _, files in os.walk(folder_path):
+            for file in files:
+
+                file_path = os.path.join(root, file)
+                encrypt_file(file_path, key)
+                os.remove(file_path)
+
+    except Exception as e:
+        print(e)
+
+encrypt_folder(folder, key)
